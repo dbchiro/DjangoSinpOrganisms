8c21598f00160ec735f385d9a2e68a23e7a229bb
Fred C <fcloitre@dbwildlife.info>
2024-04-09 09:48:26 +0200

    WIP tests

--- sinp_organisms/tests.py
+++ sinp_organisms/tests.py
@@ -1,30 +1,37 @@
+import uuid
+
 from django.test import TestCase
 from sinp_nomenclatures.models import Nomenclature
 
 from .models import Organism
 
+org1_uuid = uuid.uuid4()
+
 
 class OrganismTestCase(TestCase):
     fixtures = [
-        "initdata.xml",
+        "nomenclatures.json",
     ]
 
     def setUp(self):
         Organism.objects.create(
             label="Organism 1",
             short_label="ORG1",
-            action_scope=Nomenclature.objects.filter(
+            uuid=org1_uuid,
+            action_scope=Nomenclature.objects.get(
                 type__mnemonic="action_scope", code="reg"
-            ).first(),
-            status=Nomenclature.objects.filter(
+            ),
+            status=Nomenclature.objects.get(
                 type__mnemonic="organism_status", code="pub"
-            ).first(),
-            type=Nomenclature.objects.filter(
+            ),
+            type=Nomenclature.objects.get(
                 type__mnemonic="organism_type", code="pubestab"
-            ).first(),
+            ),
         )
 
     def test_organisms(self):
         """Animals that can speak are correctly identified"""
         org1 = Organism.objects.get(short_label="ORG1")
         self.assertEqual(str(org1), "ORG1")
+        self.assertEqual(org1.uuid, org1_uuid)
+        self.assertEqual(org1.natural_key(), (org1_uuid,))
--- tests/test_organisms.py
+++ tests/test_organisms.py
@@ -9,6 +9,8 @@ org_uuid = uuid.uuid4()
 
 
 class OrganismTestCase(TestCase):
+    fixtures = ["sinp_organisms/fixtures/nomenclatures.json"]
+
     def setUp(self):
         Organism.objects.create(
             uuid=org_uuid,
@@ -16,11 +18,15 @@ class OrganismTestCase(TestCase):
             label="MUSEUM NATIONAL D'HISTOIRE NATURELLE",
             short_label="MNHN",
             action_scope=Nomenclature.objects.get(
-                type__mnemonic="action_scope"
-            )[0],
+                type__mnemonic="action_scope", code="nat"
+            ),
+            status=Nomenclature.objects.get(
+                type__mnemonic="organism_status", code="pub"
+            ),
         )
 
-    def test_animals_can_speak(self):
+    def organismExists(self):
         """Animals that can speak are correctly identified"""
         org1 = Organism.objects.get(uuid=org_uuid)
         self.assertEqual(org1.short_label, "MNHN")
+        self.assertEqual(org1.natural_key(), (org_uuid,))
